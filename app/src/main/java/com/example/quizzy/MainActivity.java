01efa1bef8a25bae95ebc7078755c25d4a441751
JAINAM <rajshah71202@gmail.com>
2023-07-30 11:22:28 +0530

    Stage2

---
+++ app/src/main/java/com/example/quizzy/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.quizzy;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuKt;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.quizzy.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseFirestore database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        database = FirebaseFirestore.getInstance();
+
+        ArrayList<CategoryModel> categories = new ArrayList<>();
+
+        CategoryAdapter adapter = new CategoryAdapter(this, categories);
+
+        database.collection("categories")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        categories.clear();
+                        for (DocumentSnapshot snapshot : value.getDocuments()) {
+                            CategoryModel model = snapshot.toObject(CategoryModel.class);
+                            model.setCategoryId(snapshot.getId());
+                            categories.add(model);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+        binding.categoryList.setLayoutManager(new GridLayoutManager(this, 2));
+        binding.categoryList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.wallet) {
+            Toast.makeText(this, "wallet is clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
